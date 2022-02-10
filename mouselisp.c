54b2c3a7152b1b7206d13a660fd66f341fb49c6e
Itoh Shimon <shimon@postfixnotation.org>
2022-02-10 22:47:48 +0900

    dump mode

--- mouselisp.c
+++ mouselisp.c
@@ -10,5 +10,15 @@ int main(int argc, char **argv) {
     return 0;
   }
 
-  return 0;
+  if (argc > 2 && strcmp(argv[1], "--dump") == 0) {
+    ml_parser parser =
+        (ml_parser){.file = (ml_file){.str = argv[2]}};
+    ml_object *root = ml_parser_parse(&parser);
+    if (root == NULL)
+      fatal("parse");
+    ml_object_debug_dump(root);
+    return 0;
+  }
+
+  return 1;
 }
