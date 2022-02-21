f90db534f7a33310e7da8fb04cd1c91b62d150b0
Itoh Shimon <shimon@postfixnotation.org>
2022-02-21 16:03:40 +0900

    simplify.

--- arith.c
+++ arith.c
@@ -15,7 +15,7 @@ int chk_muli(ml_machine *m, int a, int b) {
 }
 
 int chk_addi(ml_machine *m, int a, int b) {
-  if ((b > 0 && a > (INT_MAX - b)) || (b < 0 && a < (INT_MIN - b)))
+  if ((a > 0 && b > (INT_MAX - a)) || (a < 0 && b < (INT_MIN - a)))
     ml_throw(m, ml_arith_error(m, the_nil));
 
   return a + b;
@@ -29,14 +29,14 @@ int chk_subi(ml_machine *m, int a, int b) {
 }
 
 int chk_divi(ml_machine *m, int a, int b) {
-  if ((b == 0) || (a == INT_MIN && b == -1))
+  if ((b == 0) || (b == -1 && a == INT_MIN))
     ml_throw(m, ml_arith_error(m, the_nil));
 
   return a / b;
 }
 
 int chk_modi(ml_machine *m, int a, int b) {
-  if ((b == 0) || (a == INT_MIN && b == -1))
+  if ((b == 0) || (b == -1 && a == INT_MIN))
     ml_throw(m, ml_arith_error(m, the_nil));
 
   return a % b;
